673bb07e7fd8ffb49e76ffe6ec00328df8af0324
alex-mj <gboxmj@gmail.com>
2021-09-10 15:57:22 +0300

    protorype work

---
+++ go.mod
@@ -0,0 +1,15 @@
+module fcm-go-admin-sdk-proto
+
+go 1.16
+
+require (
+	cloud.google.com/go v0.94.1 // indirect
+	cloud.google.com/go/firestore v1.5.0 // indirect
+	firebase.google.com/go v3.13.0+incompatible
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	golang.org/x/net v0.0.0-20210908191846-a5e095526f91 // indirect
+	golang.org/x/sys v0.0.0-20210908233432-aa78b53d3365 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/api v0.56.0
+	google.golang.org/genproto v0.0.0-20210903162649-d08c68adba83 // indirect
+)
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	firebase "firebase.google.com/go"
+	messaging "firebase.google.com/go/messaging"
+	"google.golang.org/api/option"
+)
+
+func main() {
+	//------------------------------------------------------------------
+	fmt.Println("Hello, I'm prototype FCM sending")
+
+	// 1) download private key (.json) from https://console.firebase.google.com/
+	//  ->Project Settings ->Service accounts  save to local file /cred/cred.json
+	// (dont forget add cred.json to .gitignore)
+
+	opt := option.WithCredentialsFile("./cred/cred.json")
+	app, err := firebase.NewApp(context.Background(), &firebase.Config{
+		// TODO: change it
+		ProjectID: "YOU'R-PROJECT-NAME"}, opt)
+	if err != nil {
+		fmt.Printf("error initializing app: %v", err)
+	}
+	//------------------------------------------------------------------
+	fmt.Println("I'll try to send a message")
+	// Obtain a messaging.Client from the App.
+	ctx := context.Background()
+	client, err := app.Messaging(ctx)
+	if err != nil {
+		log.Fatalf("error getting Messaging client: %v\n", err)
+	}
+	// 2) You must add the mobile app
+	// https://console.firebase.google.com/
+	// ->Project Settings ->General
+	// and get a registration token from the mobile device
+	// example:
+	// registrationToken = "cqzrxJjSQD29FAOBDzyBP5:APA91bF7A7Z_f21_m5XGgwM17lcoiNEGX5sUqfpygnd2alokQ1nWdDI23xE4q1Hv0taT12jaQ00mTWzgGuBVyDO9yyvfsxkq47iOaYcQPyfjuvpw5qmDByO7lLzr471RNHFCtMS_ALmH"
+
+	// TODO: change it
+	registrationToken := "registration_token_from_you'r_device"
+
+	// See documentation on defining a message payload.
+	message := &messaging.Message{
+		Data: map[string]string{
+			"score": "850",
+			"time":  "2:45",
+		},
+		Token: registrationToken,
+	}
+
+	// Send a message to the device corresponding to the provided
+	// registration token.
+	response, err := client.Send(ctx, message)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// Response is a message ID string.
+	fmt.Println("Successfully sent message:", response)
+}
